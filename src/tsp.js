01cc181f638da1d4f2f8e4eace16536f3eec6d97
rmrotek <remek87@gmail.com>
2018-11-01 13:55:35 +0100

    render

--- src/tsp.js
+++ src/tsp.js
@@ -125,4 +125,37 @@ export default class TSP {
     })
     return distance
   }
+
+  render () {
+    let {ctx, r, nodes, dr} = this
+    ctx.clearRect(0, 0, this.width * dr, this.height * dr)
+
+    ctx.lineWidth = this.lw * dr
+    ctx.strokeStyle = 'rgba(64, 64, 64, 0.2)'
+
+    // lines
+    this.orders.concat(this.orders[0]).reduce((a, b) => {
+      //console.log(a, '->', b)
+      let na = nodes[a]
+      let nb = nodes[b]
+      ctx.beginPath()
+      ctx.moveTo(na.x * dr, na.y * dr)
+      ctx.lineTo(nb.x * dr, nb.y * dr)
+      ctx.stroke()
+      return b
+    })
+
+    ctx.lineWidth = 1 * dr
+    ctx.strokeStyle = '#900'
+    ctx.fillStyle = '#f66'
+    // nodes
+    nodes.map(n => {
+      ctx.beginPath()
+      ctx.arc(n.x * dr, n.y * dr, r * dr, 0, 2 * Math.PI)
+      ctx.fill()
+      ctx.stroke()
+    })
+    $('#gen').html(this.ga.generation)
+    //$('#mutation').html(this.ga.mutation_count)
+  }
 }
